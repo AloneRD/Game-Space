dcafce812c93cb0beefc9d62d577b7e360f62699
Artur <artur_goncharov_97@mail.ru>
2022-06-14 09:37:03 +0300

    Fix flicker starts

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import curses
 import asyncio
+import time
 
 TIC_TIMEOUT = 0.1
 
@@ -7,20 +8,19 @@ TIC_TIMEOUT = 0.1
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.refresh()
-        for i in range(20):
+        for _ in range(int(2*10)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for i in range(3):
+        for _ in range(int(0.3*10)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for i in range(5):
+        for _ in range(int(0.5*10)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for i in range(3):
+        for _ in range(int(0.3*10)):
             await asyncio.sleep(0)
 
 
@@ -32,8 +32,9 @@ def draw(canvas):
         coroutines.append(blink(canvas, 5, 20+(i*2)))
     while True:
         for coroutine in coroutines:
-            coroutine.send(None)
             canvas.refresh()
+            coroutine.send(None)
+        time.sleep(TIC_TIMEOUT)
 
 
     #  while True:
@@ -49,8 +50,6 @@ def draw(canvas):
     #     canvas.addstr(row,colum,"*")
     #     time.sleep(0.3)
     #     canvas.refresh()
-
-
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
