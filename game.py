edc801c0847db7ace32b87e57ff1bf1037a77d80
Artur <artur_goncharov_97@mail.ru>
2022-08-16 23:26:43 +0300

    added animation of spaceship

--- game.py
+++ game.py
@@ -1,31 +1,37 @@
+from asyncio.log import logger
 import curses
 import asyncio
 import time
 import logging
 import random
 
+from itertools import cycle
+
+from curses_tools import draw_frame
+
 logging.basicConfig(filename="sample.log", level=logging.INFO)
 
 TIC_TIMEOUT = 0.1
 
 
+async def count_delay(seconds):
+    for _ in range(int(seconds * 10)):
+        await asyncio.sleep(0)
+
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(random.randint(0, 20)):
-            await asyncio.sleep(0)
+        await count_delay(2)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(random.randint(0, 3)):
-            await asyncio.sleep(0)
+        await count_delay(0.3)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(random.randint(0, 5)):
-            await asyncio.sleep(0)
+        await count_delay(0.5)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(random.randint(0, 3)):
-            await asyncio.sleep(0)
+        await count_delay(0.3)
 
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.6, columns_speed=0):
@@ -58,24 +64,34 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.6, columns_speed=0
         column += columns_speed
 
 
+async def animate_spaceship(canvas, start_row, start_column, animation_spaceship):
+    for frame in cycle(animation_spaceship):
+        draw_frame(canvas, start_row, start_column, frame)
+        await count_delay(0.3)
+        draw_frame(canvas, start_row, start_column, frame, negative=True)
+
+
 def draw(canvas):
     curses.curs_set(False)
     canvas.nodelay(True)
 
     height, width = canvas.getmaxyx()
+
+    with open('animations\\rocket_frame_1.txt', 'r') as frame_1, open('animations\\rocket_frame_2.txt', 'r') as frame_2:
+        animation_spaceship = [frame_1.read(), frame_2.read()]
+
     coroutines = [blink(canvas, x, y, symbol) for x, y, symbol in generate_stars(height, width)]
     coroutines.append(fire(canvas, height/2, width/2))
-    for coroutine in coroutines.copy():
-        while True:
-            for coroutine in coroutines:
+    coroutines.append(animate_spaceship(canvas, height/2, width/2-2, animation_spaceship))
+
+    while True:
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
                 canvas.refresh()
-                try:
-                    coroutine.send(None)
-                except StopIteration:
-                    coroutines.remove(coroutine)
-            time.sleep(TIC_TIMEOUT)
-            if len(coroutines) == 0:
-                break
+            except StopIteration:
+                coroutines.remove(coroutine)
+        time.sleep(TIC_TIMEOUT)
 
 
 def generate_stars(height: int, width: int, count_stars=100):
